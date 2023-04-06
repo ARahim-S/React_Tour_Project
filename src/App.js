f3096bde0168a56e7e74a624b817fe577a787044
ARahim-S <arahim.s@yandex.com>
2023-04-06 23:47:39 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Welcome React</h1>
+      <p>Tour Project in here</p>
+    </div>
+  );
 }
 
 export default App;
